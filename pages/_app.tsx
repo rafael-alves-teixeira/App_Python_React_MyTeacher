43bfcc1c77e7822960899dcb48665d6f4e10f52b
rafael-alves-teixeira <rafael.info.tc@gmail.com>
2022-10-06 23:44:36 -0300

    Frontend repository

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Cabecalho from '../src/components/Cabecalho/Cabecalho'
+import tema from '../src/themes/theme'
+import { ThemeProvider } from '@mui/material'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={tema}>
+      <Cabecalho></Cabecalho>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ src/components/Cabecalho/Cabecalho.style.tsx
@@ -0,0 +1,16 @@
+import { styled } from "@mui/material";
+
+
+export const CabecalhoContainer = styled('header')`
+    width: 230px;
+    margin: 40px auto;
+    text-align: center;
+
+    border: 2px solid green;
+
+`;
+
+export const Logo = styled('img')`
+    width: 100%;
+    background-color: yellow;
+`;
